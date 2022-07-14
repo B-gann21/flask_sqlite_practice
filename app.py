15d91c9bcc210534206e1c563a6530b1e76b7354
Brylan Gannon <brylan.gannon112@gmail.com>
2022-07-14 09:49:20 -0600

    WIP

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
